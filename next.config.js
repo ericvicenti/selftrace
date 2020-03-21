b4e4574d9e8636fc17d2083c64bc57a73f7ff27a
Anar Kafkas <anarkafkas@gmail.com>
2020-03-21 03:30:14 +0300

    to avoid confusion don't gitignore next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { withExpo } = require('@expo/next-adapter');
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+const withSourceMaps = require('@zeit/next-source-maps');
+
+module.exports = withExpo(
+  withImages(
+    withFonts(
+      withSourceMaps({
+        projectRoot: __dirname,
+        webpack: (config, options) => {
+          if (!options.isServer) {
+            config.resolve.alias['@sentry/node'] = '@sentry/browser';
+            config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+          }
+          return config;
+        },
+        env: {
+          googleMapsAPIKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      })
+    )
+  )
+);
