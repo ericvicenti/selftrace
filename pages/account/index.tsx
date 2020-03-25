f38dcf0c49b79c33b84556b70714c8e796c80589
Anar Kafkas <anarkafkas@gmail.com>
2020-03-25 21:55:14 +0300

    improve account page

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 // import fetch from 'isomorphic-fetch';
 import { NextPageContext } from 'next';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { t } from 'i18n-js';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -9,12 +9,13 @@ import Router from 'next/router';
 import { AuthStatus } from '../../data-types';
 import Text from '../../components/Text';
 import PageContainer from '../../components/PageContainer';
+import Hoverable from '../../components/Hoverable';
 import TabNavigator from '../../components/TabNavigator';
 import SubmitButton from '../../components/SubmitButton';
 import * as SignoutActions from '../../actions/auth/signout';
 import { Action, Dispatch } from '../../actions';
 import { ReduxRoot } from '../../reducers';
-import { Colors, Margins } from '../../styles';
+import { Colors, Margins, Shadows } from '../../styles';
 import FlexLoader from '../../components/FlexLoader';
 
 const styles = StyleSheet.create({
@@ -26,6 +27,17 @@ const styles = StyleSheet.create({
   formContainer: {
     width: '100%',
     marginTop: Margins.Y,
+  },
+  itemsContainer: {
+    marginTop: Margins.MAX_Y,
+  },
+  item: {
+    ...Shadows.FORM_CONTAINER,
+    shadowRadius: 10,
+    alignSelf: 'flex-start',
+    marginBottom: Margins.MIN_Y,
+    minWidth: 180,
+    minHeight: 50,
   },
 });
 
@@ -74,12 +86,14 @@ function AccountPage({ signoutUser, pathname, authStatus, progress, clearProgres
       <TabNavigator pathname={pathname} />
       <PageContainer>
         <Text style={styles.title}>{t('headers.account')}</Text>
-        <TouchableOpacity onPress={onLinkPress('/account/profile')}>
-          <Text>{t('screens.account.profile')}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onLinkPress('/account/update-password')}>
-          <Text>{t('screens.account.updatePassword')}</Text>
-        </TouchableOpacity>
+        <View style={styles.itemsContainer}>
+          <Hoverable onPress={onLinkPress('/account/profile')} style={styles.item}>
+            <Text>{t('screens.account.profile')}</Text>
+          </Hoverable>
+          <Hoverable onPress={onLinkPress('/account/update-password')} style={styles.item}>
+            <Text>{t('screens.account.updatePassword')}</Text>
+          </Hoverable>
+        </View>
         <SubmitButton
           label={t('buttons.signout')}
           progress={progress}
