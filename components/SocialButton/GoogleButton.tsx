55c9e67b5da6229a1bb63ac2644df75f1a9e4f9a
kwak123 <kwak123@gmail.com>
2020-03-25 23:16:50 -0400

    add view wrapper for shadow

--- components/SocialButton/GoogleButton.tsx
+++ components/SocialButton/GoogleButton.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
-import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { GoogleLogo } from '../../util/svg';
+import { Colors } from '../../styles';
 
 const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#EEEEEE',
+  },
   container: {
-    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     height: 40, // does not recognize dp
     paddingHorizontal: 8,
-    borderBottomColor: 'black',
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    borderRadius: 2,
+    shadowRadius: 5,
+    shadowOpacity: 0.5,
+    shadowColor: Colors.SHADOW.toString(),
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   logo: {
     marginRight: 24,
   },
   text: {
+    // TODO: Move this to Colors
     color: 'rgba(0,0,0,0.54)',
+    fontFamily: "'Roboto', sans-serif;",
   },
 });
 
 const GoogleButton = () => (
-  <TouchableOpacity style={styles.container}>
-    <GoogleLogo style={styles.logo} />
-    <Text style={styles.text}>Sign in with Google</Text>
-  </TouchableOpacity>
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.button}>
+      <GoogleLogo style={styles.logo} />
+      <Text style={styles.text}>Sign in with Google</Text>
+    </TouchableOpacity>
+  </View>
 );
 
 export default GoogleButton;
