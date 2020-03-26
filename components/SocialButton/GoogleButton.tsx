58c43d1c99b1baea3636ffa3167f6dbd7cd3ba48
kwak123 <kwak123@gmail.com>
2020-03-25 22:58:52 -0400

    apply google spacing and color branding

--- components/SocialButton/GoogleButton.tsx
+++ components/SocialButton/GoogleButton.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { GoogleLogo } from '../../util/svg';
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40, // does not recognize dp
+    paddingHorizontal: 8,
+    borderBottomColor: 'black',
+  },
+  logo: {
+    marginRight: 24,
+  },
+  text: {
+    color: 'rgba(0,0,0,0.54)',
+  },
+});
+
 const GoogleButton = () => (
-  <View>
-    <GoogleLogo />
-    <Text>Sign in with Google</Text>
-  </View>
+  <TouchableOpacity style={styles.container}>
+    <GoogleLogo style={styles.logo} />
+    <Text style={styles.text}>Sign in with Google</Text>
+  </TouchableOpacity>
 );
 
 export default GoogleButton;
